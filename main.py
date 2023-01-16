c0bd8fe3ed196dad9384938a02130dc361fea266
matsialzer <bodibek1962@gmail.com>
2023-01-16 23:56:59 +0500

    1

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import logging
 from logic.bot import *
 
 if __name__ == "__main__":
-    client = telegram_parser()
+    logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w")
+    logging.debug("A DEBUG Message")
+    logging.info("An INFO")
+    logging.warning("A WARNING")
+    logging.error("An ERROR")
+    logging.critical("A message of CRITICAL severity")
+    client = telegram_parser(logging)
     client.run_until_disconnected()
