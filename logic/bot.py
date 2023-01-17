cda284d92d82b1401d02fd8fc36e2b103e80a43c
Рахматов Жасур <jasur.rakhmatov03@gmail.com>
2023-01-17 11:28:05 +0500

    d

--- logic/bot.py
+++ logic/bot.py
@@ -9,9 +9,17 @@ def telegram_parser(logging, send_message_func=None, loop=None):
 
     @client.on(events.NewMessage())
     async def handler(event):
+
         if send_message_func is None:
-            channelData = await client.get_entity(event.message.peer_id.channel_id)
-            send(channelData.username, event, logging)
+            try:
+                channelData = await client.get_entity(event.message.peer_id.channel_id)
+                if not event.photo:
+                    image = "none"
+                else:
+                    image = await client.download_media(event.photo, "img/test")
+                send(channelData.username, event, image, logging)
+            except AttributeError:
+                pass
         else:
             await send_message_func(f'@prime1\n{event.raw_text}')
 
--- logic/sender.py
+++ logic/sender.py
@@ -3,12 +3,7 @@ import requests
 from logic.settings import *
 
 
-def send(username, event, logging):
-    try:
-        image = event.photo.id
-    except AttributeError:
-        image = "none"
-
+def send(username, event, image, logging):
     payload = {
         'site': 'telegram',
         'title': 'bot',
@@ -21,4 +16,6 @@ def send(username, event, logging):
     headers = {'api-key': API_KEY}
     response = requests.request("POST", API_URL, headers=headers, data=payload, files=files)
     logging.info('new event')
-    logging.info(response.text)
+    logging.info(event.text)
+
+    # logging.info(response.text)
