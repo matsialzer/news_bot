9cd28edd6b1368996e3b60607429710451ac8b78
Рахматов Жасур <jasur.rakhmatov03@gmail.com>
2023-01-17 11:30:01 +0500

    d

--- main.py
+++ main.py
@@ -3,10 +3,5 @@ from logic.bot import *
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w")
-    logging.debug("A DEBUG Message")
-    logging.info("An INFO")
-    logging.warning("A WARNING")
-    logging.error("An ERROR")
-    logging.critical("A message of CRITICAL severity")
     client = telegram_parser(logging)
     client.run_until_disconnected()
